4de2e98a3c87cf81e2dea773a39beadcac81a76b
unknown <hello@erickouassi.com>
2021-09-11 15:10:42 -0400

    Added new codes and comments

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
+// Do the math and calculation
 function compute()
 {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var year = new Date().getFullYear()+parseInt(years);
+    var amount = principal * years * rate /100;
+    
     p = document.getElementById("principal").value;
     
+    document.getElementById("result").innerHTML="If you deposit <mark>$"+principal+" </mark>,\
+    <br\>at an interest rate of <mark> "+rate+"%\</mark>.<br\>You will receive an amount of <mark>$"+amount+" </mark>,\
+    <br\>in the year <mark>"+year+"\</mark>.<br\>"
+    
 }
-        
+
+// Update the slider 
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+// Validate the iput amount to only accept positive number
+function validAmount() 
+{
+    var principal = document.getElementById("principal").value;
+    if (principal < 0) 
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+}      
