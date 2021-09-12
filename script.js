250abdbd0d52cc29aabd7ef2c9aea58c01c0ecc4
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2021-09-11 23:37:13 -0400

    Update code

--- script.js
+++ script.js
@@ -7,8 +7,15 @@ function compute()
     var year = new Date().getFullYear()+parseInt(years);
     var amount = principal * years * rate /100;
     
-    p = document.getElementById("principal").value;
+    // Validate the iput amount to only accept positive number
+    if(principal == "" || principal <= 0)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal");
+        return;
+    }
     
+    // Message display
     document.getElementById("result").innerHTML="If you deposit <mark>$"+principal+" </mark>,\
     <br\>at an interest rate of <mark> "+rate+"%\</mark>.<br\>You will receive an amount of <mark>$"+amount+" </mark>,\
     <br\>in the year <mark>"+year+"\</mark>.<br\>"
@@ -21,14 +28,3 @@ function updateRate()
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
-
-// Validate the iput amount to only accept positive number
-function validAmount() 
-{
-    var principal = document.getElementById("principal").value;
-    if (principal <= 0) 
-    {
-        alert("Enter a positive number");
-        document.getElementById("principal").focus();
-    }
-}      
